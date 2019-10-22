7615f7c14899a24aa99475adaa9398c78a63fda9
Alexandr Kuzmin <shurik.kuzmin@gmail.com>
2019-10-21 22:49:57 -0400

    More intro.

--- intro.py
+++ intro.py
@@ -6,5 +6,19 @@ else:
 #elif a>3:
 #    print("NADA")
 for i in range(3):
-    print("Heloo world!")
+    print("Hello world!",i)
     
+arr = [1.0, 2, "Test", 5,3.1415926,16]
+print(arr[2])
+arr[4] = 10
+print(arr)
+print(len(arr)) 
+
+for i in range(len(arr)):
+    print("i = ", i, "arr[i] = ", arr[i])
+    
+for i in range(11):
+    print(i*i)
+
+for a in arr:
+    print(a)
